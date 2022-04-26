d95779cb33c58fb4ff1b0a6a777c71b4cb0ed442
AdrianoS96 <adrianodes96@gmail.com>
2022-04-26 15:08:38 -0300

    Aula 4
    Continuação com o acréscimo de pegar a informação da quantidade dos resultados e o tempo

---
+++ Projeto02/robogoogle2.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By #importar BY para o funcionamento do comando
+from selenium.webdriver.chrome.service import Service #Para utilizar os serviços para o chromeDrive na nuvem
+from webdriver_manager.chrome import ChromeDriverManager#para gerenciar o chromedrive da nuvem
+from time import sleep
+
+pesquisa = input('Digite a sua pesquisa: ')
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-logging") 
+options.add_argument("--log-level=3")
+
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=options)
+
+driver.get('https://www.google.com.br/')
+campo = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input' )
+campo.send_keys(pesquisa)
+campo.send_keys(Keys.RETURN)
+
+resultados = driver.find_element(By.ID, 'result-stats').text
+print(resultados)
+
+sleep(10)
+#driver.quit()
